c72c5a0f05da9b01e69662d4bc6ced12e6e2e3bd
dhamer ayoub <ayoubdhamer5@gmail.com>
2022-03-06 19:39:14 +0100

    skulette du projet

---
+++ src/main/java/tn/globebusiness/spring/GlobeBusinessApplication.java
@@ -0,0 +1,13 @@
+package tn.globebusiness.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobeBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobeBusinessApplication.class, args);
+	}
+
+}
